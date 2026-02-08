f83e35075c656d6ad65899de0ff5cc0c499ebf23
maszatllc-ctrl <maszatllc@gmail.com>
2026-02-08 16:36:44 -0600

    Delete lead-buyer-funnel directory

--- lead-buyer-funnel/api/pixel-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = function handler(req, res) {
-  const pixelId = process.env.FB_PIXEL_ID || '';
-  res.setHeader('Content-Type', 'application/javascript');
-  res.setHeader('Cache-Control', 'public, max-age=3600');
-  res.status(200).send(`window.__FB_PIXEL_ID="${pixelId}";`);
-};
